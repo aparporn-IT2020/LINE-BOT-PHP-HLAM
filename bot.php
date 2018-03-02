e1a2716605789eb57bc424038438e933ae5854b0
aparporn <antbeecat123@gmail.com>
2018-03-02 14:04:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,13 +18,9 @@ function CreatePost ($replyToken,$messages,$data,$access_token){
    echo $result . "";
 }
 
-function GetData (){  
- $fiarray = explode("\n", file_get_contents('Shopping.txt'));
-  return $fiarray;
-}
- 
 
 
+  
 echo "Hi , I'm shopping bot ";
 $access_token = '8fsfZxbwQLZGFtqc1rU/JGRsr4FHbXPpN8xv2v2cR4ry5mGgYzwsAUBvmx51ozB3e1NlNQuW7fFI8babhjaGeceCxNkQTKAdkpzXOy7/3phOil6V54Ft5yhpd/dGhpu1x4NcjbeXgPboFTQcGCJXdgdB04t89/1O/w1cDnyilFU=';
 
@@ -38,12 +34,12 @@ if (!is_null($events['events'])) {
  // Loop through each event	
  foreach ($events['events'] as $event) {		
   // Reply only when message sent is in 'text' format		
-  if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			
-    $fileArr = GetData();
-      echo $fileArr;
+  if ($event['type'] == 'message' && $event['message']['type'] == "F") {			
+    $fiarray = explode("\n", file_get_contents('Shopping.txt'));
+      
    // Get text sent			
    //$text = $event['message']['text'];			
-     $text = 'OKKK';			
+     $text = $fiarray[0];			
    // Get replyToken			
    $replyToken = $event['replyToken'];			
    // Build message to reply back			
