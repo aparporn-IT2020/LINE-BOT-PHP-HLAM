6a7d19f5015ef9cb0049c839bf5965f8a12c63ed
aparporn <antbeecat123@gmail.com>
2018-03-02 09:52:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,7 @@
 <?php
             
-            $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+            $access_token = 'TNpAB3x76j50mLoyeassUyuHx5fv1QtD6jiWNPHID4LiG+0l/tln/t1WnR+4Jz7a/aB11L8M+UJVqUo2UbRFNM7LqtJ8FVUGvS0CxPqQ/PDYmLCWT/lemeI6zqhlNqEQE5cZxw5iCd6m8Hz2zOKSIQdB04t89/1O/w1cDnyilFU=
+';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
