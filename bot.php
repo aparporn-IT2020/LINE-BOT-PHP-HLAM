7596e7688469d163313adb1e7c7eca23cfff89c6
Aparporn <70187291+aparporn-IT2020@users.noreply.github.com>
2020-08-31 09:10:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ use LINE\LINEBot\Event\UnfollowEvent;
 use LINE\LINEBot\Event\UnknownEvent;
 use LINE\LINEBot\Exception\InvalidEventRequestException;
 use LINE\LINEBot\Exception\InvalidSignatureException;
-new \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+new LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('6zDMyMWoEbyMb0inVnCxNeglFVxuDjbX7S3V1fq0cvnGwHHHliSwJ3a/bSIERUAdc+lWr4chqBXbwGJT9HnZGTDAUQUGAg0O58NaiDN/83GzJ4R7Fa/FimarNBwZ+eW3zRDrv9B4/j/8hKmNJep9cgdB04t89/1O/w1cDnyilFU=');
