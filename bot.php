80d1e64b9fae3c22945ffd755d4e994edd56c01b
Aparporn <70187291+aparporn-IT2020@users.noreply.github.com>
2020-09-02 16:22:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ try {
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('6zDMyMWoEbyMb0inVnCxNeglFVxuDjbX7S3V1fq0cvnGwHHHliSwJ3a/bSIERUAdc+lWr4chqBXbwGJT9HnZGTDAUQUGAg0O58NaiDN/83GzJ4R7Fa/FimarNBwZ+eW3zRDrv9B4/j/8hKmNJep9cgdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '0126e35ca29d722a11fab40b4948db24']);
 } catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+    echo $e->getMessage();
 }
 
 function CreatePost ($data){
