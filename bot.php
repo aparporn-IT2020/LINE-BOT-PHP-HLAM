317b1df2c0b39d6afa33533813a5ab3075dcd03f
Aparporn <70187291+aparporn-IT2020@users.noreply.github.com>
2020-09-02 14:27:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@
 function CreatePost ($data){
    	$url = 'https://api.line.me/v2/bot/message/push';
    	$access_token = '6zDMyMWoEbyMb0inVnCxNeglFVxuDjbX7S3V1fq0cvnGwHHHliSwJ3a/bSIERUAdc+lWr4chqBXbwGJT9HnZGTDAUQUGAg0O58NaiDN/83GzJ4R7Fa/FimarNBwZ+eW3zRDrv9B4/j/8hKmNJep9cgdB04t89/1O/w1cDnyilFU=';
-   	$headers = array('Content-Type: application/json; charser=UTF-8', 'Authorization: Bearer ' . $access_token);	
+   	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);	
    	$post = json_encode($data);			
 
    	$ch = curl_init();	
@@ -67,8 +67,7 @@ $events = json_decode($content, true);
 $ImageLink = 'https://developers.line.biz/media/homepage-products/products-messaging-api-sprite.png';
 $VDOLink = 'https://mokmoon.com/videos/Brown.mp4';
 
-try
-{
+
 		if (!is_null($events['events'])) 
 	{	
 		foreach ($events['events'] as $event) 
@@ -102,10 +101,7 @@ try
 		}		
 	}
 }
-catch (Exception $e) 
-{
-	echo "Error " . $e ."\n";
-}
+
 echo "Hi , I'm HLAM bot ";
 
 ?>
